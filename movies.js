884bcdd1babc8844dc785961c8d59988eeda6f61
Daniel <dmadera0@gmail.com>
2021-01-03 13:59:29 -0700

    filenamechange

---
+++ movies.js
@@ -0,0 +1,22 @@
+
+
+fetch('http://localhost:3000/movies')
+.then(response => response.json())
+.then(movies => render(movies))
+    
+    
+const movieList = document.querySelector(".wrapper")
+
+function render(movies) {
+movies.forEach(movie => {
+    const movieElement = document.createElement('div')
+    movieElement.innerHTML = `<a href="movie.html?id=${movie.id}"><img src="${baseImageURL}${movie.image}"></a>`
+    
+    const title = document.createElement('div')
+    title.innerText = movie.title
+    title.className = "text"
+
+    movieElement.appendChild(title)
+    movieList.appendChild(movieElement)
+})
+}
